69caaad59a1c91a27e23b39fd5214099c888acb7
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2025-09-09 10:55:00 +0530

    create ios project

---
+++ clients/ios/Whisp/Whisp/WhispApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhispApp.swift
+//  Whisp
+//
+//  Created by Krushn Dayshmookh on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WhispApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
